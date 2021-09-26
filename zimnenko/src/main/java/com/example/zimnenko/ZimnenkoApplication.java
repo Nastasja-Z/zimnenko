b34d31e0fd40597465e03a9a1bfb514e7e752f07
Nastasja-Z <anastasia.zimnenko@gmail.com>
2021-09-26 14:50:46 +0300

    Initial commit

---
+++ zimnenko/src/main/java/com/example/zimnenko/ZimnenkoApplication.java
@@ -0,0 +1,13 @@
+package com.example.zimnenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZimnenkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZimnenkoApplication.class, args);
+	}
+
+}
---
+++ zimnenko/src/test/java/com/example/zimnenko/ZimnenkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zimnenko;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZimnenkoApplicationTests {
+
+	@Ignore
+	void contextLoads() {
+	}
+
+}
