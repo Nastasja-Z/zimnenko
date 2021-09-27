e20dbdc306483f4c828aa43137d1e8e48b877ab7
Nastasja-Z <anastasia.zimnenko@gmail.com>
2021-09-27 15:01:32 +0300

    Initial commit

--- zimnenko/src/main/java/com/example/zimnenko/ZimnenkoController.java
+++ zimnenko/src/main/java/com/example/zimnenko/ZimnenkoController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class HelloWorldController {
+public class ZimnenkoController {
 
-    @GetMapping({"/", "/helloworld"})
+    @GetMapping({"/", "/zimnenko"})
     public String helloworld() {
         return "helloworld";
     }
